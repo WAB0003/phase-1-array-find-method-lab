58495ed0b54581b6fde57ad5fa9dc69b8fc9d280
Bill Brown <bill.a.brown90@gmail.com>
2023-02-26 14:02:10 -0800

    Using the Find Method to iterate through and array

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+]
+
+//create a function to handle the entire array using the find method
+// function superbowlWin(array){
+//     const found = array.find(result);
+//     if (found){
+//         return found.year
+//     }
+// }
+
+// //create a function to handle each object within the array
+// function result(object) {
+//     return object.result === "W"
+        
+//     } 
+
+// console.log(superbowlWin(record))
+
+function superbowlWin(array) {
+    const found = array.find(result)
+    console.log (found.year)
+    return found.year
+}
+
+const result = (object) => {
+    return object.result === "W"
+}
+
+console.log(superbowlWin(record))
